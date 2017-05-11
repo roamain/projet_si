cc048694588c8832125e476a0642c3539b1c3046
Pierrick Bignet <pierrick.bignet@gmail.com>
2017-05-11 23:12:15 +0200

    Initial commit

---
+++ bot.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js')
+const config = require('./config.js')
+const client = new Discord.Client()
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.username}!`)
+})
+
+client.on('message', msg => {
+  // Check if the message has been posted in a channel where the bot operates
+  // and that the author is not the bot itself
+  if (msg.channel.type !== 'dm' && (!config.channels[msg.channel.id] || msg.author.id === client.user.id)) return
+
+  // If message is hello, post hello too
+  if (msg.content === 'hello') {
+    msg.channel.sendMessage('Hello to you too !')
+  }
+})
+
+client.login(config.token)
---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  token: 'MzEyMzM0OTg0MDk2Nzc2MjAz.C_ZkNg.JJdD1ne8avZDl7w9PVaM85igqVw',
+  channel: 'channelid'
+}
