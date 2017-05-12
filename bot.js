ab1dc0d62861bc618b0a20d08f6806508d37549d
Pierrick Bignet <pierrick.bignet@gmail.com>
2017-05-12 09:47:04 +0200

    Test error

--- bot.js
+++ bot.js
@@ -4,12 +4,12 @@ const client = new Discord.Client()
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`)
-})
+});
 
 client.on('message', msg => {
   // Check if the message has been posted in a channel where the bot operates
   // and that the author is not the bot itself
-  if (msg.channel.type !== 'dm' && (!config.channels[msg.channel.id] || msg.author.id === client.user.id)) return
+  if (msg.channel.type !== 'dm' && (config.channel !== msg.channel.id || msg.author.id === client.user.id)) return
 
   // If message is hello, post hello too
   if (msg.content === 'hello') {
