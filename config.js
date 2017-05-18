1da1d97362a67d61d828fdee566e9fa5e49d32e2
Pierrick <pierrick.bignet@gmail.com>
2017-05-18 20:34:41 +0200

    Deploy Test

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  token: 'MzEyMzM0OTg0MDk2Nzc2MjAz.C_ZkNg.JJdD1ne8avZDl7w9PVaM85igqVw',
+  token: 'MzE0ODMyNDIzMTM5NjcyMDY2.C_96DQ.G0yuhZiuxSjBW61-S7-V34eauqc',
   channel: 'channelid'
 }
