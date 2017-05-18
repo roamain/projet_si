fe2529f72a9e8743c1f5332d16eac28c614c8594
Pierrick <pierrick.bignet@gmail.com>
2017-05-18 22:09:50 +0200

    Test

--- bot.js
+++ bot.js
@@ -13,6 +13,7 @@ client.on('message', msg => {
 
   // If message is hello, post hello too
   if (msg.content === 'hello') {
+    console.log('Ouais!')
     msg.channel.sendMessage('Hello to you too, student ! I\'m on Azure !')
   }
 })
