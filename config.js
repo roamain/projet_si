b30c265b5cd011ed000609fde46c8957ca2fe930
romainczn13 <romain_couzon13@hotmail.fr>
2017-06-12 18:02:17 +0200

    Create config.js

--- config.js
+++ config.js
@@ -1,4 +1,10 @@
 module.exports = {
   token: 'MzE0ODMyNDIzMTM5NjcyMDY2.C_96DQ.G0yuhZiuxSjBW61-S7-V34eauqc',
   channel: 'channelid'
+  
+  //OpenWeatherMap
+
+Openweathermap_apikey:'cf1bb87b13a1c343101030c6d7714a33',
 }
+
+
